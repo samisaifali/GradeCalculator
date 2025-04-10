52642303fe0340d24eaf73cb03ecb8240a788163
samisaifali <sali263@myseneca.ca>
2025-04-10 17:58:38 -0400

    added popup

--- grades.cpp
+++ grades.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <vector>
+#include <string>
+#include <windows.h>
 
 using namespace std;
 
@@ -13,50 +15,52 @@ double calculateGrade(const vector<double>& scores, const vector<double>& weight
     return (weightSum == 0) ? 0 : total / weightSum;
 }
 
+// Show a popup with the prompt, then collect input in terminal
+template <typename T>
+T prompt(const string& message) {
+    MessageBoxA(NULL, message.c_str(), "Input", MB_OK);
+    T value;
+    cout << message;
+    cin >> value;
+    return value;
+}
+
 int main() {
-    int numAssignments, numQuizzes, numExams;
-    vector<double> assignments, quizzes, exams;
-    vector<double> assignmentWeights, quizWeights, examWeights;
-    double score, weight;
-    
-    cout << "Enter number of assignments: ";
-    cin >> numAssignments;
+    int numAssignments = prompt<int>("Enter number of assignments:");
+    int numQuizzes = prompt<int>("Enter number of quizzes:");
+    int numExams = prompt<int>("Enter number of exams:");
+
+    vector<double> assignments, assignmentWeights;
+    vector<double> quizzes, quizWeights;
+    vector<double> exams, examWeights;
+
     for (int i = 0; i < numAssignments; ++i) {
-        cout << "Enter score for assignment " << i + 1 << ": ";
-        cin >> score;
+        double score = prompt<double>("Enter score for assignment " + to_string(i + 1) + ":");
+        double weight = prompt<double>("Enter weight for assignment " + to_string(i + 1) + " (in decimal):");
         assignments.push_back(score);
-        cout << "Enter weight for assignment " << i + 1 << " (in decimal form): ";
-        cin >> weight;
         assignmentWeights.push_back(weight);
     }
-    
-    cout << "Enter number of quizzes: ";
-    cin >> numQuizzes;
+
     for (int i = 0; i < numQuizzes; ++i) {
-        cout << "Enter score for quiz " << i + 1 << ": ";
-        cin >> score;
+        double score = prompt<double>("Enter score for quiz " + to_string(i + 1) + ":");
+        double weight = prompt<double>("Enter weight for quiz " + to_string(i + 1) + " (in decimal):");
         quizzes.push_back(score);
-        cout << "Enter weight for quiz " << i + 1 << " (in decimal form): ";
-        cin >> weight;
         quizWeights.push_back(weight);
     }
-    
-    cout << "Enter number of exams: ";
-    cin >> numExams;
+
     for (int i = 0; i < numExams; ++i) {
-        cout << "Enter score for exam " << i + 1 << ": ";
-        cin >> score;
+        double score = prompt<double>("Enter score for exam " + to_string(i + 1) + ":");
+        double weight = prompt<double>("Enter weight for exam " + to_string(i + 1) + " (in decimal):");
         exams.push_back(score);
-        cout << "Enter weight for exam " << i + 1 << " (in decimal form): ";
-        cin >> weight;
         examWeights.push_back(weight);
     }
-    
+
     double finalGrade = (calculateGrade(assignments, assignmentWeights) +
                          calculateGrade(quizzes, quizWeights) +
                          calculateGrade(exams, examWeights)) / 3;
-    
-    cout << "Final Grade: " << finalGrade << "%" << endl;
-    
+
+    string result = "Final Grade: " + to_string(finalGrade) + "%";
+    MessageBoxA(NULL, result.c_str(), "Final Grade", MB_OK);
+
     return 0;
 }
